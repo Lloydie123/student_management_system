8060ca59e9a252b95080980ce900b53de8beb9cf
Jhon Lloyd L. Borlagdan <jhonlloydborlagdan8@gmail.com>
2023-12-30 22:05:05 +0800

    Fixed the live error displaying ofr registeration

--- actions/register.php
+++ actions/register.php
@@ -4,9 +4,7 @@ require_once '../config.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-
     // get data from the form here 
-
     $lrn = $_POST['lrn'];
     $email = $_POST['email'];
     $contact_number = $_POST['contact_number'];
@@ -15,25 +13,24 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     // Validate input here 
     if (empty($lrn) || empty($email) || empty($contact_number) || empty($password) || empty($confirm_password)) {
-        
-        $error = "All fields are required";
-        header("Location: ../register.php");
+        $response = array('status' => 'error', 'message' => 'All fields are required');
     } elseif ($password !== $confirm_password) {
-        $error = "Passwords do not match";
-        header("Location: ../register.php");
+        $response = array('status' => 'error', 'message' => 'Passwords do not match');
     } else {
         // Hash the password (you should use a secure hashing method)
         $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
         // Insert user data into the database
-        $stmt = $pdo->prepare("INSERT INTO users (lrn, email, contact_number, password, type, status) VALUES (?, ?, ?, ?, 'user', 'active')");
+        $stmt = $pdo->prepare("INSERT INTO users (lrn, email, contact_number, password, type, status) VALUES (?, ?, ?, ?, 'user', 1)");
         $stmt->execute([$lrn, $email, $contact_number, $hashed_password]);
 
-        // Redirect to login page
-        // contain an error message in a variable and alert it in the login.php
-        header("Location: ../login.php");
-        exit();
+        $response = array('status' => 'success', 'message' => 'Registration is successful');
+        echo json_encode(['status' => 'success']);
+        header('Location: ../login.php');
     }
+
+    // Send JSON response back to the client-side
+    header('Content-Type: application/json');
+    echo json_encode($response);
 }
 ?>
-
--- register.php
+++ register.php
@@ -15,68 +15,144 @@
 <script>
 $(document).ready(function() {
     // Function to check if the email already exists
-    function checkEmailExists(email) {
-        // Make an AJAX request to your server-side script
-        $.ajax({
-            
-            type: "POST",
-            url: "actions/check_account.php", // Replace with the actual path to your server-side script
-            data: { email: email },
-            success: function(response) {
-                var data = JSON.parse(response);
-                if (data.status === "exists") {
-                    // Email already exists, show an error message
-                    $("#email-error").text("Email already exists").css("color", "red");
-                } else {
-                    // Email is unique, clear the error message
-                    $("#email-error").text("Email is available to use").css("color", "green");
+        function checkEmailExists(email) {
+            // Make an AJAX request to your server-side script
+            $.ajax({
+                type: "POST",
+                url: "actions/check_account.php", // Replace with the actual path to your server-side script
+                data: { email: email },
+                success: function(response) {
+                    var data = JSON.parse(response);
+                    if (data.status === "exists") {
+                        // Email already exists, show an error message
+                        $("#email-error").text("Email already exists").css("color", "red");
+                    } else if (email.indexOf('@') === -1) {
+                        // Email does not contain '@', show an error message
+                        $("#email-error").text("Please provide a valid email address").css("color", "red");
+                    } else {
+                        // Email is unique, clear the error message
+                        $("#email-error").text("Email is available to use").css("color", "green");
+                    }
                 }
-            }
-        });
-    }
+            });
+        }
 
-    function checkLRNExists(lrn) {
+        function checkLRNExists(lrn) {
         // Make an AJAX request to your server-side script
         $.ajax({
-            
             type: "POST",
             url: "actions/check_account.php", // Replace with the actual path to your server-side script
             data: { lrn: lrn },
             success: function(response) {
                 var data = JSON.parse(response);
+
                 if (data.status === "exists") {
                     // LRN already exists, show an error message
                     $("#lrn-error").text("LRN already exists").css("color", "red");
+                } else if (lrn.length !== 12 || !/^\d+$/.test(lrn)) {
+                    // LRN does not have 12 digits or contains non-numeric characters
+                    $("#lrn-error").text("Please provide a valid 12-digit LRN").css("color", "red");
+                } else if (/[^a-zA-Z0-9]/.test(lrn)) {
+                    // LRN contains special characters
+                    $("#lrn-error").text("Please remove any special characters").css("color", "red");
                 } else {
                     // LRN is unique, clear the error message
                     $("#lrn-error").text("LRN is available to use").css("color", "green");
- 
                 }
             }
         });
     }
 
     function checkContactNumberExists(contact_number) {
-        
         // Make an AJAX request to your server-side script
-        $.ajax({    
+        $.ajax({
             type: "POST",
             url: "actions/check_account.php", // Replace with the actual path to your server-side script
             data: { contact_number: contact_number },
             success: function(response) {
                 var data = JSON.parse(response);
                 if (data.status === "exists") {
-                    // LRN already exists, show an error message
+                    // Contact number already exists, show an error message
                     $("#contact_number-error").text("Contact number already exists").css("color", "red");
-               
+                } else if (contact_number.length !== 11 || !/^\d+$/.test(contact_number)) {
+                    // Contact number does not have 11 digits or contains non-numeric characters
+                    $("#contact_number-error").text("Please provide a valid 11-digit contact number").css("color", "red");
                 } else {
-                    // LRN is unique, clear the error message
-                    $("#contact_number-error").text("Contact Number is available to use").css("color", "green");
+                    // Contact number is unique, clear the error message
+                    $("#contact_number-error").text("Contact number is available to use").css("color", "green");
                 }
             }
         });
     }
 
+
+        // Function to check if password and confirm password match
+
+    
+    function checkPasswordMatch() {
+        var password = $("input[name='password']").val();
+        var confirmPassword = $("input[name='confirm_password']").val();
+         // Check if the password is strong and complies with ISO27001 Compliant Password Policy
+        var strongRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        // Check if the password contains simple words to avoid
+        var simpleWords = ["password", "123456", "qwerty"];
+
+        
+        // Check if password and confirm password match
+        if (password !== confirmPassword) {
+            // Password and confirm password do not match, show an error message
+            $("#check-password-label").text("Password and confirm password do not match").css("color", "red");
+        } else {
+            // Password and confirm password match, clear the error message
+            $("#check-password-label").text("").css("color", "green");
+        }
+    }
+
+    function checkPasswordCompliance(password) {
+        // Check if the password is strong and complies with ISO27001 Compliant Password Policy
+        var strongRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+        // Check if the password contains simple words to avoid
+        var simpleWords = ["password", "123456", "qwerty"];
+
+        // Check for compliance with the strong password policy
+        if (!strongRegex.test(password)) {
+            // Password is not strong, show an error message
+            $("#password-error").text("Password shall contain uppercase letters, lowercase letters, digits, and special characters with a minimum length of 8 characters").css("color", "red");
+            return false;
+        }
+
+        // Check for simple words
+        for (var i = 0; i < simpleWords.length; i++) {
+            if (password.toLowerCase().includes(simpleWords[i])) {
+                // Password contains a simple word, show an error message
+                $("#password-error").text("Avoid using simple words like 'password', '123456', or 'qwerty'").css("color", "red");
+                return false;
+            }
+        }
+
+        // Password is compliant, clear the error message
+        $("#password-error").text("Password is available to use").css("color", "green");
+        return true;
+    }
+
+
+    // Event listener for the password input field on keyup
+    $("input[name='password']").on("keyup", function() {
+        // Get the entered password value
+        var password = $(this).val();
+
+        // Check if the password complies with ISO27001 Compliant Password Policy
+        checkPasswordCompliance(password);
+    });
+
+
+    // Event listener for the confirm password input field on keyup
+    $("input[name='confirm_password']").on("keyup", function() {
+        // Call the function to check if password and confirm password match
+        checkPasswordMatch();
+    });
+    // Add another function here that live checks if the password is not matched, name of fucntion is check password and the function gets the value of password and confirm password, after the user types the confirm password then it checkcs if the password and confirm password is the same, if not it has a check-password-label is color red and it says "Password and confirm password is not the same"
     // Event listener for the email input field on keyup
     $("input[name='email']").on("keyup", function() {
         
@@ -104,9 +180,9 @@ $(document).ready(function() {
 
     $("input[name='contact_number']").on("keyup", function() {
         
-        // Get the entered lrn value
+        // Get the entered contact number 
         var contact_number = $(this).val();
-        // Check if the lrn is not empty
+        // Check if the contact number is not empty
         if (contact_number !== "") {
            
             // Call the function to check if the lrn already exists
@@ -118,7 +194,7 @@ $(document).ready(function() {
     // Event listener for form submission
     $("form").on("submit", function(event) {
         // Check if there are any error messages
-        if ($("#email-error").text() !== "Email is available to use" || $("#lrn-error").text() !== "LRN is available to use" || $("#contact_number-error").text() !== "Contact Number is available to use") {
+        if ($("#email-error").text() !== "Email is available to use" || $("#lrn-error").text() !== "LRN is available to use" || $("#contact_number-error").text() !== "Contact number is available to use") {
             // Prevent form submission if there are errors
             event.preventDefault();
               // Configure the alert message here
@@ -127,7 +203,34 @@ $(document).ready(function() {
                     title: 'Oops...',
                     text: "Please fix the errors before submitting the form.",
                 });
-            }
+            } else {
+        // If no errors, proceed with form submission
+            $.ajax({
+                type: "POST",
+                url: "actions/register.php",
+                data: $("form").serialize(), // Serialize the form data
+                success: function(response) {
+                    if (response.status === "success") {
+                        // Registration is successful, show a success alert
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Success',
+                            text: response.message,
+                        });
+                    } else {
+                        // Registration failed, show an error alert
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Oops...',
+                            text: response.message,
+                        });
+                    }
+                },
+                dataType: "json"
+            });
+        }
+
+
         });
     
     });
@@ -157,9 +260,12 @@ $(document).ready(function() {
         <input type="number" name="contact_number" required><br>
 
         <label>Password:</label>
+        <span id="password-error" style="color: black; margin-bottm: 20px;"></span>
         <input type="password" name="password" required><br>
 
+
         <label>Confirm Password:</label>
+        <span id="check-password-label" style="color: black; margin-bottm: 20px;"></span>
         <input type="password" name="confirm_password" required><br>
 
         <div class="form-header" >
